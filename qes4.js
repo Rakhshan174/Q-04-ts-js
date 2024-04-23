9ca4a2bab7fd6983c90d3669ebfde3ebb1514d00
Rakhshan174 <160410026+Rakhshan174@users.noreply.github.com>
2024-04-23 05:21:44 -0700

    Q 04 ts/js 45 assignment

---
+++ qes4.js
@@ -0,0 +1,10 @@
+"use strict";
+/*Find a quote form a famous person you admire. Print the quote and the name of its author.
+Your output should look somthing like the following, including the quotation marks: Albert
+Einstein once said "A person who never made a mistake never tried anything new."
+const myQuote: string = `Albert Einstein once said,"A person who never made a mistake never
+tried anything new."`*/
+// backtick
+let quote = "A Person who never made a mistake never tried anything new.";
+let author = "Albert Einstein";
+console.log(`${author} once said,"${quote}"`);
---
+++ qes4.ts
@@ -0,0 +1,16 @@
+/*Find a quote form a famous person you admire. Print the quote and the name of its author.
+Your output should look somthing like the following, including the quotation marks: Albert
+Einstein once said "A person who never made a mistake never tried anything new."
+const myQuote: string = `Albert Einstein once said,"A person who never made a mistake never
+tried anything new."`*/
+
+// backtick
+
+let quote: string = "A Person who never made a mistake never tried anything new.";
+let author: string = "Albert Einstein";
+
+console.log(`${author} once said,"${quote}"`);
+
+
+
+
